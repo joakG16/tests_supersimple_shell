6a01a9d6806eda3d7c36362568db4cb8ed1b21d7
joakG16 <4379@holbertonschool.com>
2022-08-04 11:18:34 -0700

    -

--- acce.c
+++ acce.c
@@ -1,11 +1,6 @@
 #include<stdio.h>
 #include<unistd.h>
 
-// This program places the name of the current directory in the
-// buffer array, then displays the name of the current directory
-// on the screen. Passing NULL as the buffer forces getcwd to allocate
-// memory for the path, which allows the code to support file paths
-// longer than _MAX_PATH, which are supported by NTFS.
 
 int main(void)
 {
--- getcwd.c
+++ getcwd.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// This program places the name of the current directory in the
+// buffer array, then displays the name of the current directory
+// on the screen. Passing NULL as the buffer forces getcwd to allocate
+// memory for the path, which allows the code to support file paths
+// longer than _MAX_PATH, which are supported by NTFS.
+
 int main(void)
 {
 	char *cwd = NULL;
